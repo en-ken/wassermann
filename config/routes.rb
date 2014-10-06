157e425c97414dd0527ae924a7c0e817ef95467f
Misa Takeba <e1416mt@aiit.ac.jp>
2014-10-06 10:22:41 +0000

    Add explore routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  get '/explore' => 'menu#explore'
+  get '/explore' => 'menus#explore'
+  get '/result' => 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
