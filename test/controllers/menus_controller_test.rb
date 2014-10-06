b4eae765c50795f8e97e27d7f6925fce2b8d5904
Misa Takeba <e1416mt@aiit.ac.jp>
2014-10-06 10:31:37 +0000

    Add menu controller test

--- test/controllers/menus_controller_test.rb
+++ test/controllers/menus_controller_test.rb
@@ -4,4 +4,9 @@ class MenusControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "get redirectcode when get explore path" do
+    get :explore
+    assert_response :redirect
+  end
 end
