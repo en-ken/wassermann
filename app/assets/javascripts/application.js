45a910b17104f4fcf49f375a01a6aa78f504f03e
esodov <esodov+github@gmail.com>
2014-10-09 14:12:34 +0000

    Fix conflicts.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,11 @@
 //= require twitter/bootstrap
 //= require turbolinks
 //= require_tree .
+
+$(function(){
+  $("#choice button").click(function(){
+    $(this).toggleClass("btn-primary");
+    $(this).toggleClass("btn-lg");
+  });
+});
+
