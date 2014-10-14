258839dc2e6d4f8b04bec68f178b50930d293505
esodov <esodov+github@gmail.com>
2014-10-14 01:40:32 +0000

    Add menus controller test.
    -index explore呼び出し時の検証
    -画面遷移テスト

--- test/controllers/menus_controller_test.rb
+++ test/controllers/menus_controller_test.rb
@@ -1,17 +1,31 @@
 require 'test_helper'
 
-class MenusControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-
-  test "get redirectcode when get explore path" do
-    get :explore
-    assert_response :redirect
+Class MenusControllerTest < ActionController::TestCase
+  #Page Validation
+  test 'should get index' do
+    get :index
+    assert_response :success
   end
 
-  test "get redirect_to suggest" do
+  test 'index title should be aimai-lunch' do
+    get :index
+    assert_select 'title', 'aimai-lunch'
+  end
+
+  test 'index should render correct template and layout' do
+    get :index
+    assert_template :index
+    assert_template layout: "layouts/applicaiton"
+  end
+
+  #Redirect Validation
+  test 'explore should redirect to index if there is no args' do
     get :explore
+    assert_redirected_to index_path
+  end
+
+  test 'explore should redirect to suggest if there are args' do
+    get :explore, :judge 'yes'
     assert_redirected_to suggest_path
   end
 end
