f7ccaaf6a35daf1a0c66ae7990c2833c056f1f61
esodov <esodov+github@gmail.com>
2014-10-15 12:45:31 +0000

    delete static structure test.

--- test/controllers/menus_controller_test.rb
+++ test/controllers/menus_controller_test.rb
@@ -1,31 +1,14 @@
 require 'test_helper'
 
-Class MenusControllerTest < ActionController::TestCase
-  #Page Validation
-  test 'should get index' do
-    get :index
-    assert_response :success
-  end
-
-  test 'index title should be aimai-lunch' do
-    get :index
-    assert_select 'title', 'aimai-lunch'
-  end
-
-  test 'index should render correct template and layout' do
-    get :index
-    assert_template :index
-    assert_template layout: "layouts/applicaiton"
-  end
-
+class MenusControllerTest < ActionController::TestCase
   #Redirect Validation
   test 'explore should redirect to index if there is no args' do
-    get :explore
+    get :explore, :feel => nil
     assert_redirected_to index_path
   end
 
   test 'explore should redirect to suggest if there are args' do
-    get :explore, :judge 'yes'
+    get :suggest, :feel => 'light'
     assert_redirected_to suggest_path
   end
 end
