ad4931aae69f764c56ff6de1677185d553fd6e11
Misa Takeba <e1416mt@aiit.ac.jp>
2014-10-29 10:41:10 +0000

    駅名を静的に持つよう修正

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -17,7 +17,13 @@ class MenusController < ApplicationController
   end
 
   def suggest
-    @locations = Location.all
+    @locations = Array.new
+    loc_names = ['東京','有楽町','新橋','浜松町','田町','品川','大崎','五反田','目黒','恵比寿','渋谷','原宿','代々木','新宿','新大久保','高田馬場','目白','池袋','大塚','巣鴨','駒込','田端','西日暮里','日暮里','鶯谷','上野','御徒町','秋葉原','神田']
+    loc_names.each do |loc_name|
+      location = Location.new
+      location.name = loc_name
+      @locations.push(location)
+    end
     @menu = params[:menu]
   end 
   
