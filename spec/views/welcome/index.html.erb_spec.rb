3ff4522c9e7f44e68137fb3af2325d2dc71285bb
Esodov <esodov@gmail.com>
2014-11-12 21:17:18 +0900

    Delete view test.

--- spec/views/welcome/index.html.erb_spec.rb
+++ spec/views/welcome/index.html.erb_spec.rb
@@ -1,11 +1,4 @@
 require 'spec_helper'
 
 describe "welcome/index.html.erb", :type => :view do
-  before do
-    render
-  end
-
-  it "should display title" do
-    expect(rendered).to include("あいまいランチ")
-  end
 end
