e237536d2ecc94bfe1bdc56865e495c7527962e4
yggotx <foxnoodle510@gmail.com>
2014-12-08 20:47:11 +0900

    Create looks controller

--- app/controllers/looks_controller.rb
+++ app/controllers/looks_controller.rb
@@ -1,30 +1,24 @@
 class LooksController < ApplicationController
 
-	@loc_name #場所のデータ保持用(indexからView経由でselectに渡す)
-	@freewords #外見の検索用
+  @loc_name #場所のデータ保持用(indexからView経由でselectに渡す)
+  @freewords #外見の検索用
 
-	#「雰囲気で選ぶ」選択後の処理
-	def index
-		@loc_name = params[:location]
-		@looks = get_img_urls
-	end
+  #「雰囲気で選ぶ」選択後の処理
+  def index
+    @loc_name = params[:location]
+    @freewords = get_freewords
+  end
 
-	#雰囲気選択後の遷移
-	def select
-		@freewords = get_freewords
-		redirect_to shops_url(:loc_name => params[:loc_name], :freewords => params[:freewords])
-	end
+  #雰囲気選択後の遷移
+  def select
+    redirect_to shops_url(:loc_name => params[:loc_name], :freewords => params[:freewords])
+  end
+
+  private #===== 以下プライベートメソッド =====
 	
-	private #===== 以下プライベートメソッド =====
-
-	#DBからimg_urlを全て取得
-	def get_img_urls
-		return Looks.pluck(:img_url)
-	end
-
-	def get_freewords
-		looks = Looks.find_by(looks:img_rul)
-		return looks.freewords 
-	end
+  #DBからfreewordsを取得
+  def get_freewords
+    return Looks.pluck(:freewords)
+  end
 
 end
