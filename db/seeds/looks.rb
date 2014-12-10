0a35297bcc4dbc9531ea83e970f17413f0818605
Misa Takeba <e1416mt@aiit.ac.jp>
2014-12-10 09:56:07 +0000

    Add four records on looks

--- db/seeds/looks.rb
+++ db/seeds/looks.rb
@@ -3,4 +3,12 @@ Looks.delete_all
 looks = Looks.new
 looks.img_url = "/images/tamura.jpg"
 looks.freewords = "割烹,懐石料理,小料理"
+looks.img_url = "/images/meat.jpg"
+looks.freewords = "すきやき,しゃぶしゃぶ,焼き肉,ハンバーグ"
+looks.img_url = "/images/goch.jpg"
+looks.freewords = "精進料理,激辛,オーガニック料理,チーズ"
+looks.img_url = "/images/graduate.jpg"
+looks.freewords = "インテリ,スマート,DHA"
+looks.img_url = "/images/kasuya.jpg"
+looks.freewords = "ゲテモノ,不思議,謎"
 looks.save
